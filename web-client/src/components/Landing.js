d82b57f1c609b300ea286e972ff0f7b64a1a4b2e
Piyush <44617356+piyush-shandilya@users.noreply.github.com>
2020-11-08 01:19:58 +0530

    login done

---
+++ web-client/src/components/Landing.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+// import Paper from "@material-ui/core/Paper";
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Accio Malicio</h1>
+
+        <h1>About Us</h1>
+        <h2>Piyush Shandilya</h2>
+        <h2>Prabhsimar Singh Taneja</h2>
+      </div>
+    );
+  }
+}
