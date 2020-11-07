0eedef4885dad9aca88c073ff635dd48ae2378ec
Piyush <44617356+piyush-shandilya@users.noreply.github.com>
2020-11-08 03:46:56 +0530

    details displayed on client

--- web-client/src/pages/home.js
+++ web-client/src/pages/home.js
@@ -9,7 +9,7 @@ export default class home extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      
     }
   }
   componentDidMount() {
@@ -22,6 +22,23 @@ export default class home extends Component {
     .catch((error) => {
       console.error('Error:', error);
     });
+
+    fetch('http://127.0.0.1:5000/report')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({report : {data}})
+      console.log(this.state)
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+    // var positives = []
+    // for(ele in this.state.report.data.scans){
+    //   if(this.state.report.data.scans[ele].detected == true){
+    //     positives.push({antivirus : {ele}})
+    //   }
+    // }
   }
   render() {
     return (
@@ -30,34 +47,36 @@ export default class home extends Component {
         {/* Todo */}
         {/* {user email} */}
         <Typography color="textSecondary" variant='h2' gutterBottom>
-          p4firebase@gmail.com
+          {this.state.user}
         </Typography>
         <h1> Latest Email </h1>
         <Card>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
-              sender - .........
+              sender - {this.state.sender}
             </Typography>
             <Typography variant="h5" component="h2">
-              subject :- fasdafs
+              subject :- {this.state.subject}
             </Typography>
             <Typography color="textSecondary">
               hashes found
+              sha1 :- {this.state.sha1}
+              sha256 :- {this.state.sha256}
+              md5 :- {this.state.md5}
             </Typography>
             <Typography color="textSecondary">
-              urls found
-            </Typography>
-            <Typography color="textSecondary">
-              hashes found
+              {this.state.url}
             </Typography>
 
             <Typography variant="body2" component="p">
               Attachments
               <br />
-              Report of Attachment
+              {this.state.attachment}
             </Typography>
           </CardContent>
         </Card>
+
+        
 
       </div>
     )
